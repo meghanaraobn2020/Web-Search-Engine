e122b6db6be5eb653d9df353482edb2ff08b944f
Mad1294 <56818426+Mad1294@users.noreply.github.com>
2021-01-07 20:51:28 +0100

    Svg files issue resolved &one image per doc

--- download.py
+++ download.py
@@ -1,6 +1,7 @@
 import requests
 import re
 import os
+import random
 
 
 def retrieve_text(topic):
@@ -8,6 +9,7 @@ def retrieve_text(topic):
     data_flag = True
     image_flag = True
     inp_filename = ""
+    counter = 0
     try:
         text_config = {
             'action': 'query',
@@ -46,7 +48,7 @@ def retrieve_text(topic):
         }
         num_image_response = requests.get('https://en.wikipedia.org/w/api.php', params=num_image_config).json()
 
-        # now that we havae the number of images in the page, we ask for the images that are in the page with the title
+        # now that we have the number of images in the page, we ask for the images that are in the page with the title
         image_config = {
             'action': 'query',
             'format': 'json',
@@ -57,14 +59,10 @@ def retrieve_text(topic):
         image_response = requests.get('https://en.wikipedia.org/w/api.php', params=image_config).json()
         image_page = next(iter(image_response['query']['pages'].values()))
 
-        # and we  write the image files one by one in the currect directory
-        # we also dont write the svg files, since as they are mostly the logos
-        # modily the filename_pattern regex for to accept the proper files
-
         # print(image_page)
         filename_pattern = re.compile(".*\.(?:jpe?g|gif|png|JPE?G|GIF|PNG)")
 
-        print("Starting to write Images...")
+
         for i in range(len(image_page['images'])):
 
             url_config = {
@@ -76,15 +74,13 @@ def retrieve_text(topic):
             }
             url_response = requests.get('https://en.wikipedia.org/w/api.php', params=url_config).json()
             url_page = next(iter(url_response['query']['pages'].values()))
-            print(url_page['imageinfo'][0]['url'])
-            temp = str(url_page['imageinfo'][0]['url'])
-            if temp.endswith('.svg'):
-                print("SVG image")
-                raise Exception("")
-            if filename_pattern.search(url_page['imageinfo'][0]['url']):
 
-                print("writing image " + url_page['imageinfo'][0]['url'].rsplit("/", 1)[1])
-                with open("../Dataset/" + url_page['imageinfo'][0]['url'].rsplit("/", 1)[1], 'wb') as handle:
+            if filename_pattern.search(url_page['imageinfo'][0]['url']) and (counter == 0):
+                print("Starting to write Images...")
+                counter = counter + 1
+                temp = inp_filename.rsplit(".", 1)[0] + "." + (url_page['imageinfo'][0]['url'].rsplit("/", 1)[1]).rsplit(".", 1)[1];
+                print("Image File Name: ", temp)
+                with open(temp, 'wb') as handle:
                     response = requests.get(url_page['imageinfo'][0]['url'], stream=True)
 
                     if not response.ok:
@@ -99,10 +95,10 @@ def retrieve_text(topic):
     except:
         print("No image to download")
         image_flag = False
-    if image_flag == False and data_flag == True:
+    if (image_flag == False and data_flag == True) or (data_flag == True and counter == 0):
         if os.path.exists(inp_filename):
             os.remove(inp_filename)
-            print("File remoed :", inp_filename)
+            print("File removed :", inp_filename)
         else:
             print("The file does not exist")
         return False
@@ -130,35 +126,44 @@ def FNameparser(path, start, end):
 
 
 def main():
-    # path = "Wikipedia_topics.txt"
     path = "Wikipedia_topics"
-    start = 301
-    end = 302  # 311
+    start = 3000
+    end = 8000  # 311
     orig, Namelist = FNameparser(path, start, end)
     print(orig)
     print(Namelist)
     executions = len(Namelist)
-    i = 0
+    i = 1
     correct = 0
-    while i < executions:
+    total_executions = 1000 #Change it to 1000
+    systemRandom = random.SystemRandom()
+    while correct < total_executions:
+        index = systemRandom.randint(0, len(Namelist)-1)
         print("\n###################################")
-        print("Downloading for Topic:", Namelist[i])
-        topic = str(Namelist[i])
+        print("Execution Number: ", i)
+        print("Topics Downloaded: ", correct)
+        print("Current Index: ", index)
+        print("Downloading for Topic:", Namelist[index])
+        topic = str(Namelist[index])
         topic = topic.strip()
         if retrieve_text(topic):
+            Namelist.pop(index)
             print("Download Complete")
             correct += 1
         else:
             print("Skipping the current topic")
             end = end + 1
+            Namelist.pop(index)
             _, temp = FNameparser(path, end, end + 1)
             print("Searching another topic")
             Namelist.append(temp[0])
             executions += 1
         i += 1
+
         print("###################################")
     print("\n Document Searched:", i)
     print("Document Downloaded:", correct)
+    print("End Index", end)
 
 
 if __name__ == '__main__':
